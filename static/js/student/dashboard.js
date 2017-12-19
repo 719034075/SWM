f4430486c670a8762d12c80b44e3921b84b5586e
zeuc <719034075@qq.com>
2017-12-19 18:43:05 +0800

    ## 2017-12-19
    + 用户端基本完成

---
+++ static/js/student/dashboard.js
@@ -0,0 +1,15 @@
+/**
+ * Created by 71903 on 2017/12/19.
+ */
+
+layui.use('carousel', function () {
+    var carousel = layui.carousel;
+    //建造实例
+    carousel.render({
+        elem: '#student_dashboard'
+        , height: '100%'
+        , interval: '3000'
+        , width: '100%' //设置容器宽度
+        , arrow: 'always' //始终显示箭头
+    });
+});
